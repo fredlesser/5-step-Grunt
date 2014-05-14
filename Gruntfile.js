4e2f193bb5a4eec5cd05f7486d5287ceb36f0923
fredlesser <fredericklesser@gmail.com>
2014-05-14 15:55:58 +0200

    first commit

---
+++ Gruntfile.js
@@ -0,0 +1,70 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    
+    
+    sass: {
+      options: {
+        includePaths: ['scss']
+      },
+      dist: {
+        options: {
+          outputStyle: 'compressed'
+        },
+        files: {
+          'style.css': 'scss/styles.scss'
+        }        
+      }
+    },
+
+    concat: {
+      options: {
+        separator: ';',
+      },
+      dist: {
+        src: [
+          'js/main.js',
+          'js/app.js'
+        ],
+
+        dest: 'js/main.min.js',
+      },
+
+    },
+    
+    uglify: {
+      dist: {
+        files: {
+          'js/main.min.js': ['js/main.js',
+          'js/app.js'                   
+          ]
+        }
+      }
+    },
+    
+    watch: {
+      grunt: { files: ['Gruntfile.js'] },
+      
+      javascripts: {
+        files: ['js/main.js'],
+        tasks: ['uglify']
+      },
+      
+      sass: {
+        files: 'scss/**/*.scss',
+        tasks: ['sass']
+      }
+    },
+
+
+  });
+
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  grunt.registerTask('build', ['sass']);
+  grunt.registerTask('default', ['concat', 'uglify', 'watch']);
+
+}
